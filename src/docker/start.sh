75d7ef2c6eca150c9d459265a58a994fae9091a4
Clémence Lesné <clemence.lesne@microsoft.com>
2023-04-07 12:50:03 +0200

    Fix: Clean-up the stopped agents from the remote agents list

--- src/docker/start.sh
+++ src/docker/start.sh
@@ -6,17 +6,10 @@ if [ -z "$AZP_URL" ]; then
   exit 1
 fi
 
-if [ -z "$AZP_TOKEN_FILE" ]; then
-  if [ -z "$AZP_TOKEN" ]; then
-    echo 1>&2 "error: missing AZP_TOKEN environment variable"
-    exit 1
-  fi
-
-  AZP_TOKEN_FILE=/azp/.token
-  echo -n $AZP_TOKEN >"$AZP_TOKEN_FILE"
+if [ -z "$AZP_TOKEN" ]; then
+  echo 1>&2 "error: missing AZP_TOKEN environment variable"
+  exit 1
 fi
-
-unset AZP_TOKEN
 
 if [ -n "$AZP_WORK" ]; then
   mkdir -p "$AZP_WORK"
@@ -30,18 +23,13 @@ print_header() {
 
 print_header "Configuring agent..."
 
-# Allow the agent to run as root (only feasible because the agent is running in a not-reused container)
-export AGENT_ALLOW_RUNASROOT="1"
-# Let the agent ignore the token env variables
-export VSO_AGENT_IGNORE=AZP_TOKEN,AZP_TOKEN_FILE
-
 bash config.sh \
   --acceptTeeEula \
   --agent "${AZP_AGENT_NAME:-$(hostname)}" \
   --auth PAT \
   --pool "${AZP_POOL:-Default}" \
   --replace \
-  --token $(cat "$AZP_TOKEN_FILE") \
+  --token "$AZP_TOKEN" \
   --unattended \
   --url "$AZP_URL" \
   --work "${AZP_WORK:-_work}"
@@ -49,4 +37,4 @@ bash config.sh \
 print_header "Running agent..."
 
 # Running it with the --once flag at the end will shut down the agent after the build is executed
-exec bash run-docker.sh "$@" --once
+bash run-docker.sh "$@" --once
