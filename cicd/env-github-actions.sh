9d94ac6dc9259ef46d2f3447dc52827d59040999
Clémence Lesné <clemence@lesne.pro>
2024-10-19 17:31:12 +0200

    fix: Local env vars sourcing for containers build

--- cicd/env-github-actions.sh
+++ cicd/env-github-actions.sh
@@ -16,44 +16,14 @@ if [ ! -f "$PIPELINE_FILE" ]; then
   exit 1
 fi
 
-# Create .env if it does not exist
-if [ ! -f ".env" ]; then
-  touch .env
-fi
-
 # Get "env" property from pipeline file
-ENV=$(yq '.env' $PIPELINE_FILE)
+ENV=$(yq '.env | to_entries | map("\(.key)=\(.value)") | .[]' $PIPELINE_FILE)
 
 # Store all properties from ENV, in environment variables
 while IFS= read -r line; do
-  # Skip empty lines
-  if [ -z "$line" ]; then
-    continue
-  fi
-
-  # Skip comments
-  if [[ $line == \#* ]]; then
-    continue
-  fi
-
-  # If there is no ":" in the line, skip it
-  if [[ $line != *":"* ]]; then
-    continue
-  fi
-
-  # Split the line into key and value, based on ":"
-  key=$(echo $line | cut -d':' -f1 | sed 's/^"//;s/"$//')
-  value=$(echo $line | cut -d':' -f2- | sed 's/^"//;s/"$//')
-
-  # Trim the values
-  key=$(echo $key | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
-  value=$(echo $value | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
-
-  # Trim double quotes
-  key=$(echo $key | sed 's/^"//;s/"$//')
-  value=$(echo $value | sed 's/^"//;s/"$//')
-
+  # Remove double quotes
+  line=$(echo $line | sed 's/\"//g')
   # Set the environment variable
-  echo "From GitHub Actions: $key=$value"
-  export "$key"="$value"
+  echo "From GitHub Actions: $line"
+  export "$line"
 done <<< "$ENV"
